4b6c3bb88f7bca5033c5582987a019d44764cc40
Jorge P. <jorgepascosoto@gmail.com>
2025-12-16 19:26:10 -0500

    finish day 11 part 2

--- 11-reactor/11-reactor-part2.py
+++ 11-reactor/11-reactor-part2.py
@@ -1,22 +1,7 @@
+from functools import lru_cache
+
 OUT = "out"
 STARTING_POINT = "svr"
-
-
-def move_to(mapper: dict, key: str, path: set[str] | None = None) -> int:
-    if path is None:
-        path = set()
-
-    if mapper[key][0] == OUT:
-        return 1
-
-    if key in path:
-        return 0
-    path.add(key)
-
-    count = 0
-    for next_ in mapper[key]:
-        count += move_to(mapper, next_, path)
-    return count
 
 
 def main(content: str):
@@ -25,11 +10,31 @@ def main(content: str):
         key, vals = line.split(":")
         vals = vals.strip().split(" ")
         mapper[key] = vals
-    result = move_to(mapper, STARTING_POINT)
-    print(f"RESULT: {result}")
+
+    visiting = set()
+
+    @lru_cache(maxsize=None)
+    def move_to(key: str, seen_dac: bool = False, seen_fft: bool = False) -> int:
+        if key == OUT:
+            return 1 if (seen_dac and seen_fft) else 0
+
+        seen_dac = seen_dac or key == "dac"
+        seen_fft = seen_fft or key == "fft"
+
+        visiting.add(key)
+        try:
+            total = 0
+            for nxt in mapper[key]:
+                total += move_to(nxt, seen_dac, seen_fft)
+            return total
+        finally:
+            visiting.remove(key)
+
+    result = move_to(STARTING_POINT)
+    print(result)
 
 
 if __name__ == "__main__":
-    with open("./example-input.txt", "r") as file:
+    with open("./input.txt", "r") as file:
         raw_content = file.read()
     main(raw_content.strip())
